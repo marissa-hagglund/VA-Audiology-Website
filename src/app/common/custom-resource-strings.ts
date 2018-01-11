301d7ee705422b774187a61201706801035bbcd2
VIEWPOINT\joseph.remington <VIEWPOINT\joseph.remington@viewpoint.com>
2018-01-10 20:34:51 -0800

    Refactor Resource Strings.

---
+++ src/app/common/custom-resource-strings.ts
@@ -0,0 +1,33 @@
+export class TsScreenerQuestionStrings {
+  question1: string = 'Have you experienced tinnitus lasting more than 2-3 minutes?';
+
+  question2: string = 'Have you experienced tinnitus for at least 6 months?';
+
+  question3: string = 'In a quiet room, can you hear tinnitus?';
+
+  question4: string = 'When you heard tinnitus this past year, was it caused by a recent event?';
+
+  question5: string = 'Does your tinnitus seem to come and go on its own, in addition to being caused by a recent event(s)';
+
+  question6: string = 'Do you experience tinnitus on a: ';
+}
+
+export class TsScreenerAnswerStrings {
+  YES: string = 'YES';
+
+  NO: string = 'NO';
+
+  ALWAYS: string = 'Always';
+
+  USUALLY: string = 'Usually';
+
+  SOMETIMES_OCCASIONALLY: string = 'Sometimes/Occasionally';
+
+  YES_ALWAYS: string = 'YES, Always';
+
+  YES_SOMETIMES: string = 'YES, Sometimes';
+
+  DAILY_OR_WEEKLY_BASIS: string = 'Daily or weekly basis';
+
+  MONTHLY_OR_YEARLY_BASIS: string = 'Monthly or yearly basis';
+}
