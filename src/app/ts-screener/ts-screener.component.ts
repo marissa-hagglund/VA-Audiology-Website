e5087ce30bc62eaa8001e89582ba0e5a6780da45
VIEWPOINT\joseph.remington <VIEWPOINT\joseph.remington@viewpoint.com>
2018-01-11 16:47:28 -0800

    Use resource strings for answer

--- src/app/ts-screener/ts-screener.component.ts
+++ src/app/ts-screener/ts-screener.component.ts
@@ -11,12 +11,12 @@ import {Router} from "@angular/router";
     <h3 style="color: white" align="center">Tinnitus Screener</h3>
     <screener-question *ngIf="currentState === 1" [question]="questionStrings.question1" (onClickedBack)="moveStateBackward()" (onClickedNext)="moveStateForward($event)"></screener-question>
     <screener-question *ngIf="currentState === 2" [question]="questionStrings.question2" (onClickedBack)="moveStateBackward()" (onClickedNext)="moveStateForward($event)"></screener-question>
-    <screener-question *ngIf="currentState === 3" [question]="questionStrings.question3" [radio1]="'Always'" [radio2]="'Usually'" [radio3]="'Sometimes/Occasionally'"
+    <screener-question *ngIf="currentState === 3" [question]="questionStrings.question3" [radio1]="answerStrings.ALWAYS" [radio2]="answerStrings.USUALLY" [radio3]="answerStrings.SOMETIMES_OCCASIONALLY"
                        (onClickedBack)="moveStateBackward()" (onClickedNext)="moveStateForward($event)"></screener-question>
-    <screener-question *ngIf="currentState === 4" [question]="questionStrings.question4" [radio1]="'YES, Always'" [radio2]="'YES, Sometimes'" [radio3]="'NO'"
+    <screener-question *ngIf="currentState === 4" [question]="questionStrings.question4" [radio1]="answerStrings.YES_ALWAYS" [radio2]="answerStrings.YES_SOMETIMES" [radio3]="answerStrings.NO"
                        (onClickedBack)="moveStateBackward()" (onClickedNext)="moveStateForward($event)"></screener-question>
     <screener-question *ngIf="currentState === 5" [question]="questionStrings.question5" (onClickedBack)="moveStateBackward()" (onClickedNext)="moveStateForward($event)"></screener-question>
-    <screener-question *ngIf="currentState === 6" [question]="questionStrings.question6" [radio1]="'Daily or weekly basis'" [radio2]="'Monthly or yearly basis'"
+    <screener-question *ngIf="currentState === 6" [question]="questionStrings.question6" [radio1]="answerStrings.DAILY_OR_WEEKLY_BASIS" [radio2]="answerStrings.MONTHLY_OR_YEARLY_BASIS"
                        (onClickedBack)="moveStateBackward()" (onClickedNext)="moveStateForward($event)"></screener-question>
   `
 })
