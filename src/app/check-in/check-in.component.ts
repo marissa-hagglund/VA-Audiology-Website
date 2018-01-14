4d6ba5da9304be87fe173a17a11bdbc067bf9399
Tutu Yin <xiaolei@pdx.edu>
2018-01-13 22:53:07 -0800

    fixed responsive issue with check-in component and appointments component; added autofocus, and error message for check-in component's input

--- src/app/check-in/check-in.component.ts
+++ src/app/check-in/check-in.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, transition } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
 
@@ -10,6 +10,8 @@ import { Router } from '@angular/router';
 
 export class CheckInComponent {
    public patientId: string = '';
+   public authenticationFlag: boolean = true;
+
    constructor(private router: Router) {};
 
    public onClick() {
@@ -20,7 +22,18 @@ export class CheckInComponent {
         this.router.navigateByUrl('/appointments');
         console.log('log in with ' + this.patientId);
       } else {
+        this.authenticationFlag = false;
+        this.patientId = '';
         console.log('failed log in ' + this.patientId);
       }
    }
-}
+
+   public keyDownFunction(event) {
+    if ( event.keyCode === 13) {
+      this.onClick();
+    } else {
+      this.authenticationFlag = true;
+    }
+  }
+
+  }
