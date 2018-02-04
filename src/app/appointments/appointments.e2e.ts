4cbf2d4857221ff6ec44fcf0f60f792c82369bbd
seanpaterson <pater2@pdx.edu>
2018-02-03 21:43:14 -0800

    idk

--- src/app/appointments/appointments.e2e.ts
+++ src/app/appointments/appointments.e2e.ts
@@ -13,22 +13,18 @@ describe('Appointments', function() {
   });
 
   it('should click Initial Assessment', async function() {
-    // browser.get('/appointments');
-    await element(by.buttonText('Initial Assessment')).click();
+    await element(by.id('one')).click();
     await expect(browser.getCurrentUrl()).toEqual('http://localhost:3000/ts');
   });
   it('should click Hearing Aids Fitting', async function() {
-    // browser.get('/appointments');
-    await element(by.buttonText('Hearing Aids Fitting')).click();
+    await element(by.id('two')).click();
     await expect(browser.getCurrentUrl()).toEqual('http://localhost:3000/ts');
   });
   it('should click Hearing Aids Follow-Up Visit', async function() {
-    // browser.get('/appointments');
-    await element(by.buttonText('Hearing Aid Follow-Up Visit')).click();
+    await element(by.id('three')).click();
     await expect(browser.getCurrentUrl()).toEqual('http://localhost:3000/ts');
   });
   it('should click home page', async function() {
-    // browser.get('/appointments');
     await element(by.css('[class="logo"]')).click();
     await expect(browser.getCurrentUrl()).toEqual('http://localhost:3000/home');
   });
