700cf5bf6fd1fb945c7c90ffbffd8944735e56d0
Tutu Yin <xiaolei@pdx.edu>
2018-02-04 12:29:43 -0800

    add router unit test in appointment and home component

--- src/app/appointments/appointments.components.spec.ts
+++ src/app/appointments/appointments.components.spec.ts
@@ -1,16 +1,20 @@
 import { AppointmentsComponent } from './appointments.component';
 import { TestBed, async, ComponentFixture } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
+// tslint:disable-next-line:whitespace
+import { RouterModule, Router, Routes } from '@angular/router';
 import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('Initial Assessment', () => {
   let component: AppointmentsComponent;
   let fixture: ComponentFixture<AppointmentsComponent>;
+  let router: Router;
+  let location: Location;
 
   beforeEach(async () => {
     TestBed.configureTestingModule({
         imports: [
-          RouterTestingModule,
+        RouterTestingModule
         ],
         providers: [
         ],
@@ -23,25 +27,40 @@ describe('Initial Assessment', () => {
     fixture = TestBed.createComponent(AppointmentsComponent);
     component = fixture.debugElement.componentInstance;
 
+    router = TestBed.get(Router);
     fixture.detectChanges();
+    router.initialNavigation();
   });
 
   it('should test for the on Initial Assessment console output', () => {
     let spy = spyOn(console, 'log');
     component.onInitialAssessment();
     expect(console.log).toHaveBeenCalled();
+
+    spy = spyOn(router, 'navigateByUrl');
+    component.onInitialAssessment();
+    expect(router.navigateByUrl).toHaveBeenCalledWith('/ts');
   });
 
   it('should test for the on Hearing Aids Fitting console output', () => {
     let spy = spyOn(console, 'log');
     component.onHearingAidsFitting();
     expect(console.log).toHaveBeenCalled();
+
+    spy = spyOn(router, 'navigateByUrl');
+    component.onHearingAidsFitting();
+    expect(router.navigateByUrl).toHaveBeenCalledWith('/ts');
   });
 
   it('should test for the on HHearing Aids Evaluation console output', () => {
     let spy = spyOn(console, 'log');
     component.onHearingAidsEvaluation();
     expect(console.log).toHaveBeenCalled();
+
+    spy = spyOn(router, 'navigateByUrl');
+    component.onHearingAidsEvaluation();
+    expect(router.navigateByUrl).toHaveBeenCalledWith('/ts');
+
   });
 
 });
