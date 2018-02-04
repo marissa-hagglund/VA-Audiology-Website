0bfc4ecdf0d4115f32f1dff198cbbd94dd5e034e
jason <xingy@pdx.edu>
2018-02-03 17:26:22 -0800

    Add new functional test file and test cases for THS component

--- src/app/home/home.e2e.ts
+++ src/app/home/home.e2e.ts
@@ -14,19 +14,21 @@ describe('Home', function() {
   /* Protractor was not waiting for the page to fully load
      thus, 'async' and 'await' is used throughtout the test.
   */
-  it('Should check title', async function() {
+  it('Should show title text', async function() {
     await expect(home.getPageTitleText()).toEqual('VA Website!');
   });
 
+  /* Both Patient Check In and Audiologist Login uses the same component
+     so, only the two buttons functionalies are tested
+  */
   it('Should navigate to Patient Login page', async function() {
     await home.getPatientCheckInBtn().click();
     await expect(browser.getCurrentUrl()).toEqual('http://localhost:3000/login');
   });
 
-  // Fail on purpose to show that the test was properly checking elements
   it('Should navigate to Audiologist Login page', async function() {
     await home.getAudiologistLoginBtn().click();
-    await expect(browser.getCurrentUrl()).toEqual('http://localhost:3000/FAIL');
+    await expect(browser.getCurrentUrl()).toEqual('http://localhost:3000/login');
   });
 
 });
