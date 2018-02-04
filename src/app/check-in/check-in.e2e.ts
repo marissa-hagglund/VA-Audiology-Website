7723b5fda4eb1804cd0c4f5c41f95faf75a91f65
seanpaterson <pater2@pdx.edu>
2018-02-03 22:02:06 -0800

    idk

--- src/app/check-in/check-in.e2e.ts
+++ src/app/check-in/check-in.e2e.ts
@@ -12,22 +12,21 @@ describe('Login', function() {
   });
 
   it('should login as patient', async function() {
-    await element(by.css('[placeholder="Usercode"]')).sendKeys('4444');
+    await element(by.css('usercode')).sendKeys('4444');
     await element(by.buttonText('Log In')).click();
     await expect(browser.getCurrentUrl()).toEqual('http://localhost:3000/appointments');
   });
   it('should login as audiologist', async function() {
-    await element(by.css('[placeholder="Usercode"]')).sendKeys('994444');
+    await element(by.id('usercode')).sendKeys('994444');
     await element(by.buttonText('Log In')).click();
     await expect(browser.getCurrentUrl()).toEqual('http://localhost:3000/audiologist');
   });
   it('return to home page', async function() {
-    browser.get('/login');
     await element(by.css('[class="logo"]')).click();
     await expect(browser.getCurrentUrl()).toEqual('http://localhost:3000/home');
   });
   it('should not login', async function() {
-    await element(by.css('[placeholder="Usercode"]')).sendKeys('9948444');
+    await element(by.id('usercode')).sendKeys('9948444');
     await element(by.buttonText('Log In')).click();
     await element(by.css('[class="failLogIn"]')).isDisplayed();
     await expect(browser.getCurrentUrl()).toEqual('http://localhost:3000/login');
