905bd39215dbd237037b42faa5473403f1d915b9
Marissa Hagglund <mdh3@pdx.edu>
2018-02-05 19:04:27 -0800

    removing point value additions since they are already incorporated into the summary

--- src/app/services/ths-data.service.ts
+++ src/app/services/ths-data.service.ts
@@ -4,7 +4,6 @@ import { Injectable } from '@angular/core';
 export class ThsDataService {
   public history: number[] = [1];
   public dataRecord: Array<{state, choice}> = [];
-  public pointRecord: number[] = [0, 0, 0];
 
   // This function will save the current state and choice the patient made for it in an Array
   // It will also keep a list of the states it has been to previously for going back and tracking progress.
@@ -15,32 +14,12 @@ export class ThsDataService {
     console.log(this.history);
     console.log(this.dataRecord);
 
-    // Adds up the total points
-    // Tinnitus section
-    if (state >= 1 && state <= 4) {
-        this.pointRecord[0] += +selection[0];
-    } else if (state >= 5 && state <= 8) { // Hearing section
-        this.pointRecord[1] += +selection[0];
-    } else if (state === 9) { // Sound Tolerance section
-        this.pointRecord[2] = +selection[0];
-    }
-  }
-
   // If it is not the first question in the process, then the record for that previous question will
   // be popped and the previous state returned
   public moveStateBackward(): number {
     if (this.history.length <= 1) {
       return null;
     }
-
-    // Tinnitus section
-    /* if (state >= 1 && state <= 4) {
-        this.pointRecord[0] -= +selection[0];
-    } else if (state >= 5 && state <= 8) { // Hearing section
-        this.pointRecord[1] -= +selection[0];
-    } else if (state === 9) { // Sound Tolerance section
-        this.pointRecord[2] = 0;
-    } */
 
     this.history.pop();
     this.dataRecord.pop();
