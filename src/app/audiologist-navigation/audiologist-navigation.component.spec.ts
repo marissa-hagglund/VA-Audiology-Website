2b3d220c3b6090928cef3ec5fcf5d3dab3b6599c
jason <xingy@pdx.edu>
2018-02-16 17:45:31 -0800

    Add a few more test and comment

--- src/app/audiologist-navigation/audiologist-navigation.component.spec.ts
+++ src/app/audiologist-navigation/audiologist-navigation.component.spec.ts
@@ -1,68 +1,92 @@
 import { AudiologistNavigationComponent } from './audiologist-navigation.component';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { NO_ERRORS_SCHEMA } from '@angular/core';
-import { RouterModule, Router, Routes } from '@angular/router';
-import { RouterTestingModule } from '@angular/router/testing';
+import { DebugElement } from '@angular/core';
 
 /*
-  Unit tests for the Audiologist Navigation comoponent
+  Unit tests for the Audiologist Navigation component
 */
-fdescribe('Audiologist Navigation', () => {
+describe('Audiologist Navigation', () => {
   let component: AudiologistNavigationComponent;
   let fixture: ComponentFixture<AudiologistNavigationComponent>;
-  let router: Router;
+  let de: DebugElement;
 
   beforeEach(async() => {
     TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
-      providers: [
-      ],
       schemas: [NO_ERRORS_SCHEMA],
       declarations: [ AudiologistNavigationComponent ]
     }).compileComponents();
 
     fixture = TestBed.createComponent(AudiologistNavigationComponent);
     component = fixture.debugElement.componentInstance;
-    router = TestBed.get(Router);
     fixture.detectChanges();
-    router.initialNavigation();
-
+    de = fixture.debugElement;
   });
 
-  fit('should create', () => {
+  it('should create', () => {
     expect(component).toBeTruthy();
   });
 
-  fit('should test for audiologist nav bar toggle console output', () => {
+  it('should test for audiologist nav bar toggle console output', () => {
     let spy = spyOn(console, 'log');
     component.onToggle();
+    fixture.detectChanges();
     expect(console.log).toHaveBeenCalled();
   });
 
-  fit('should test for audiologist default visibility', () => {
+  it('should test for audiologist landing page to be summary', () => {
     expect(component.summary).toBe(true);
   });
 
-  fit('should test for audiologist nav bar onToggle', () => {
+  /*
+  Basic unit tests for each of the methods
+  */
+  it('should test for audiologist nav bar toggle button to be false', () => {
     component.onToggle();
+    fixture.detectChanges();
     expect(component.active).toBe(false);
   });
 
-  fit('should test for audiologist showRecommendedTests', () => {
+  it('should test for audiologist recommended tests to be true', () => {
     component.showRecommendedTests();
     expect(component.recommendedTests).toBe(true);
   });
 
-  fit('should test for audiologist showSuggestedTests', () => {
+  it('should test for audiologist suggested test to be true', () => {
     component.showSuggestedTests();
     expect(component.suggestedTests).toBe(true);
   });
 
-  fit('should test for audiologist showSummary', () => {
+  it('should test for audiologist summary to be true', () => {
     component.showSummary();
     expect(component.summary).toBe(true);
+  });
+
+  /*
+    The following 3 checks to make sure that clicking on the button will triggers the method
+  */
+  it('should test for audiologist recommendedTests button click event', () => {
+    let recommended = spyOn(component, 'showRecommendedTests');
+    let recommendedBtn = de.nativeElement.querySelectorAll('.left-container .main-btn');
+    recommendedBtn.item(0).click();
+    fixture.detectChanges();
+    expect(recommended).toHaveBeenCalled();
+  });
+
+  it('should test for audiologist suggestedTests button click event', () => {
+    let suggested = spyOn(component, 'showSuggestedTests');
+    let suggestedBtn = de.nativeElement.querySelectorAll('.left-container .main-btn');
+    suggestedBtn.item(1).click();
+    fixture.detectChanges();
+    expect(suggested).toHaveBeenCalled();
+  });
+
+  it('should test for audiologist summary button click event', () => {
+    let summary = spyOn(component, 'showSummary');
+    let summaryBtn = de.nativeElement.querySelectorAll('.left-container .main-btn');
+    summaryBtn.item(2).click();
+    fixture.detectChanges();
+    expect(summary).toHaveBeenCalled();
   });
 
 });
